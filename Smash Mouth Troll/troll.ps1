3a8411f54d18607c4535448798bfeeb7192abb82
CaliNux <132522491+calinux-py@users.noreply.github.com>
2023-10-13 19:45:35 -0700

    Add files via upload

---
+++ Smash Mouth Troll/troll.ps1
@@ -0,0 +1,23 @@
+function Troll {
+    Add-Type -AssemblyName System.Windows.Forms
+
+    $lyrics = @(
+		"Somebody once told me the world is gonna roll me"
+		"I ain't the sharpest tool in the shed"
+		"She was looking kind of dumb with her finger and her thumb"
+		"In the shape of an 'L' on her forehead"
+        "Well, the years start coming and they don't stop coming",
+        "and they don't stop coming"
+    )
+
+    foreach ($line in $lyrics) {
+        if ($line -eq "and they don't stop coming") {
+            while ($true) {
+                [System.Windows.Forms.MessageBox]::Show($line, "meme", [System.Windows.Forms.MessageBoxButtons]::OK)
+            }
+        }
+        else {
+            [System.Windows.Forms.MessageBox]::Show($line, "meme", [System.Windows.Forms.MessageBoxButtons]::OK)
+        }
+    }
+}
