2064d1ea00bdbcf53e2102ee57378a432f361821
CaliNux <132522491+calinux-py@users.noreply.github.com>
2024-10-05 18:18:06 -0700

    Update To-Discord.ps1

--- To-Discord/To-Discord.ps1
+++ To-Discord/To-Discord.ps1
@@ -2,15 +2,15 @@
 
 function To-Discord {
     param (
-        [string]$webhookUrl = 'YOUR WEBHOOK'
+        [string]$webhookUrl = 'YOUR WEBHOOK HERE'
     )
     
     $output = $input | Out-String
-    $content = $output.Substring(0, [math]::Min($output.Length, 2000))
+    $content = '```' + $output.Substring(0, [math]::Min($output.Length, 1994)) + '```'
     
     if (-not [string]::IsNullOrEmpty($content.Trim())) {
         irm $webhookUrl -Method Post -ContentType 'application/json' -Body (@{content=$content} | ConvertTo-Json)
     } else {
-        write "Output is empty or not valid for sending."
+        Write-Host "Output is empty or not valid for sending."
     }
 }
